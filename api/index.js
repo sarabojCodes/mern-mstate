aa24f4388af33d0bac03957cf13f78ce1cc8b82e
Saraboj Dev <sarabojDev@gmail.com>
2024-06-25 06:43:33 +0530

    connect mongodb database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
-import express from 'express'
-const app = express()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+dotenv.config()
+const app = express();
 
-app.listen(300,()=>{
-    console.log("Server is runnig on port 3000")
-})
+mongoose
+  .connect(
+    process.env.DATABASE_URL,
+    {
+      dbName: "mern-app-collection",
+    }
+  )
+  .then(() => {
+    console.log("database connected successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+app.listen(300, () => {
+  console.log("Server is runnig on port 3000");
+});
