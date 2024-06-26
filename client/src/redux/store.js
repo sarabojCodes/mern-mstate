dca5ae97bb32224ed2153d9fa48165789a5ec0f7
Saraboj Dev <sarabojDev@gmail.com>
2024-06-26 07:02:32 +0530

    create persistore redux created

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {persistReducer} from 'redux-persist'
 import userSlice from './user/userSlice.js'
+import storage from 'redux-persist/lib/storage'
+import persistStore from 'redux-persist/es/persistStore'
+
+const rootReducer = combineReducers({
+    user:userSlice
+})
+
+const persistConfig = {
+    key:"root",
+    storage,
+    varsion:1
+}
+const persistedReducer = persistReducer(persistConfig,rootReducer)
 
 export const store = configureStore({
-  reducer: {
-   user:userSlice
-  },
+  reducer: persistedReducer,
   middleware:(getDeafultMiddleware)=>getDeafultMiddleware({
     serializableCheck:false
   })
 })
+
+export const persistor = persistStore(store)
