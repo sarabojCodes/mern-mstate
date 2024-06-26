9dc4d23583f8f774868c26cd7859de857d274e1a
Saraboj Dev <sarabojDev@gmail.com>
2024-06-26 05:44:26 +0530

    Completed signup page functions

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,11 +3,13 @@ import bycrypt from "bcrypt";
 import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res,next) => {
   const { username, email, password } = req.body;
+ 
   const hashedpassword = bycrypt.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedpassword });
   try {
     await newUser.save();
     res.status(200).json({
+      success:true,
       status: 200,
       message: "create successfully",
     });
