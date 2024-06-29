b95ec4993acf6e66076c7f40012900749d5c2032
Saraboj Dev <sarabojDev@gmail.com>
2024-06-29 20:40:00 +0530

    Update update listing section

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,34 +1,51 @@
-import listingModel from "../models/listing.model.js"
-import { errorHandler } from "../utils/error.js"
+import listingModel from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
+export const createLisling = async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const listing = await listingModel.create(req.body);
+    return res.status(200).json(listing);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
 
-export const createLisling = async(req,res,next)=>{
-     try{
-        console.log(req.body)
-         const listing = await listingModel.create(req.body)
-         return res.status(200).json(listing)
-     }catch(error){
-        console.log(error)
-        next(error)
-     }
-}
+export const deleteListing = async (req, res, next) => {
+  const listing = await listingModel.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(401, "Listing not fount"));
+  }
+  console.log(req.user.id, listing.userRef);
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can delete only your own listings!"));
+  }
+  try {
+    await listingModel.findByIdAndDelete(req.params.id);
+    res.json({
+      success: true,
+      message: "Deleted listing successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteListing  = async (req,res,next)=>{
-    const listing = await listingModel.findById(req.params.id)
-    if(!listing){
-       return next(errorHandler(401,"Listing not fount"))
-    }
-    console.log(req.user.id ,listing.userRef)
-    if(req.user.id !== listing.userRef){
-       return next(errorHandler(401,"You can delete only your own listings!"))
-    }
-    try{
-        await listingModel.findByIdAndDelete(req.params.id)
-        res.json({
-         success:true,
-         message:"Deleted listing successfully"
-        })
-    }catch(error){
-       next(error)
-    }
-}
+export const updateListing = async (req, res, next) => {
+   try {
+  const listing = await listingModel.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings"));
+  }
+ 
+    const updatedListingfunc = await listingModel.findByIdAndUpdate(req.params.id,req.body,{new:true});
+    console.log(updatedListingfunc)
+    res.json(updatedListingfunc)
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router =  express.Router()
-import {createLisling, deleteListing} from '../controllers/listing.controller.js'
+import {createLisling, deleteListing, updateListing} from '../controllers/listing.controller.js'
 import { verifyToken } from '../utils/verfiyUser.js'
 
 
 router.post("/create",verifyToken,createLisling)
 router.delete("/delete/:id",verifyToken,deleteListing)
+router.post("/update/:id",verifyToken,updateListing)
 
 export default router
